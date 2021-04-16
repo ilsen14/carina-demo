f153cf9fd67e956753ad4aca46e3989487a3cc94
ilsen14 <isenko@solvd.com>
2021-04-16 18:14:41 +0300

    Project class

--- src/main/java/com/qaprosoft/carina/demo/consant/ProjectConstants.java
+++ src/main/java/com/qaprosoft/carina/demo/consant/ProjectConstants.java
@@ -5,6 +5,4 @@ public  class ProjectConstants {
     public static final String UNIMPLEMENTED_IOS = "Unimplemented for iOS";
     public static final String UNIMPLEMENTED_ANDROID = "Unimplemented for Android";
     public static final String UNIMPLEMENTED_DESKTOP = "Unimplemented for Desktop";
-
-
 }
