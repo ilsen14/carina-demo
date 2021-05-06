ad3a860db0468f47be7945fd667bfade6ba4b150
ilsen14 <isenko@solvd.com>
2021-05-06 17:14:15 +0300

    Phone Finder

--- src/main/java/com/qaprosoft/carina/demo/gui/components/PhoneFinderMenu.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/components/PhoneFinderMenu.java
@@ -1,12 +1,28 @@
 package com.qaprosoft.carina.demo.gui.components;
 
-import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
+import com.qaprosoft.carina.core.gui.AbstractUIObject;
+import com.qaprosoft.carina.demo.gui.pages.BrandModelsPage;
 
 public class PhoneFinderMenu extends AbstractUIObject {
 
+    private final static Logger LOGGER = Logger.getLogger(PhoneFinderMenu.class);
+
+    @FindBy(xpath = "//a[text()='%s']")
+    private ExtendedWebElement openModelSearch;
+
     public PhoneFinderMenu(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
+    }
+
+    public BrandModelsPage openModelSearch(String text){
+        LOGGER.info("Apple Search Page is opened!");
+        openModelSearch.format(text).click();
+        return new BrandModelsPage(driver);
     }
 }
